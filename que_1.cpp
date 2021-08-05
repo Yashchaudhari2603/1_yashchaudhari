87784b2eb12acd1f8cc2c8f004466b6570fea53d
Yash Jayant Chaudhari <73481258+Yashchaudhari2603@users.noreply.github.com>
2021-08-05 16:55:08 +0530

    Update que_1.cpp

--- que_1.cpp
+++ que_1.cpp
@@ -2,21 +2,23 @@
 using namespace std;
 int main() {
     
-    long int n,i,divisor=0;
+    int n,i,num=0;
     cin>>n;
     if(n<0){
-    cout<<"Please provide a valid positive integer"<<endl;
-for(i=1;i<=n;++i)
-{
-if(n%i==0)
-divisor++;
-}
-
-if(divisor==3){
-num++;
-}
-
-cout<<num;
-
+        cout<<"Please provide a valid positive integer"<<endl;
+    }
+    for(i=1;i<=n;++i)
+    {
+        int divisor=0;
+        for(int j=1;j<=i;j++){
+            
+            if(i%j==0){
+                divisor++;
+            }
+        }
+        if(divisor>3)
+            num++;
+    }
+    cout<<num;
     return 0;
 }
